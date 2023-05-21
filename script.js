42fb81c7059228ab586cdfa2fc5a97d7a0e1f44c
GiorgiSarkisyan <gios48320@gmail.com>
2023-05-21 23:45:29 +0400

    huh

---
+++ script.js
@@ -0,0 +1,18 @@
+let firstName ="Giorgi"
+let lastName ="Sarkisyan"
+let Age ="17"
+let relationshipStatus ="married"
+console.log(`My name is ${firstName}. My last name is ${lastName}, I am ${Age} years old and I am ${relationshipStatus}`)
+console.log("My" + " " + "Name" + " " + "is" + " " + firstName + "." + " " + "Last" + " " + "name" + " " + "is" + " " + lastName + "." + " " + "Age" + " " + "is" + " " + Age + "." + " " + "I" + " " + "am" + " " + relationshipStatus)
+let a = 2
+let b = 5
+let c = 3
+console.log(a*b*c)
+console.log(a-b-c)
+console.log(a+b+c)
+console.log(a+c-b)
+let squareA = 7
+let squareB = 5
+let squareC = squareA
+let squareD = squareB
+console.log(squareA*squareB)
